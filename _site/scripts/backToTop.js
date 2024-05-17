2818deba6ffe9e33618beeba833a8ecc9b1545e6
Antoine Moreau <antoine9510@gmail.com>
2024-05-17 23:32:47 +0200

    deteted errors

--- _site/scripts/backToTop.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", backToTop);
-
-function backToTop(){
-    let elem = document.getElementById('back-top');
-    elem.addEventListener("click", (e) => {
-        e.preventDefault();
-        window.scrollTo(0,0);
-    });
-} 
