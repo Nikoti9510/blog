70409d39f7cb4440e5ac959b92a4a5393c700a2a
Antoine Moreau <antoine9510@gmail.com>
2024-05-19 20:15:12 +0200

    test render markdown 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
+
+    let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+    eleventyConfig.setLibrary("md", markdownIt(options));
 
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/style');
