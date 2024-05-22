9e1e84a973ef192bb3c43fb3b6588a08c13f30ce
Antoine Moreau <antoine9510@gmail.com>
2024-05-22 23:02:54 +0200

    menu burger
    - nettoyage des classes CSS de la gestion du layout
    - gestion de la logique du menu burger ok
    --> reste à faire le visuel, svg à animer

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", Menu);
+/* Variables globales */
+const navBar = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav-toggle");
 
-function Menu () {
-    /* gestion de la page active */
-    let pageUrl = window.location.href.substring(window.location.href); /* .lastIndexOf('/') + 1 */
-    links = document.querySelectorAll('.header > nav > a');
+/* Gestion de la page active */
+function activeItem () {
+    let pageUrl = window.location.href.substring(window.location.href);
+    links = document.querySelectorAll('.main-nav a');
     links.forEach((link) => {
         link.parentNode.classList.remove("current");
         if(link.href == pageUrl ) {
@@ -11,3 +13,21 @@ function Menu () {
         }
     });
 }
+
+/* Toogle menu burger au click */
+function burgerMenu () {
+    let visibility = navBar.getAttribute('data-visible');
+    if (visibility === "false") {
+        navBar.setAttribute('data-visible', 'true');
+        navBar.setAttribute('data-expanded', 'true');
+        navToggle.setAttribute('aria-expanded', 'true');
+    } else {
+        navBar.setAttribute('data-visible', 'false');
+        navBar.setAttribute('data-expanded', 'false');
+        navToggle.setAttribute('aria-expanded', 'false');
+    }
+}
+
+/* Event listener */
+document.addEventListener("DOMContentLoaded", activeItem);
+navToggle.addEventListener("click", burgerMenu);
